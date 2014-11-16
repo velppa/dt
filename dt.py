cbe5b8c5a66b5203fc8a43eb85bab70e51849a70
Pavel Popov <pavelpopov@outlook.com>
2014-11-16 15:53:29 +0300

    Work as expected

--- dt.py
+++ dt.py
@@ -13,7 +13,6 @@ LOGLEVEL=logging.DEBUG
 logging.basicConfig(level=LOGLEVEL)
 
 datestr = re.compile(r'^((?P<days>[+-]?\d+)d)?(?P<hours>\d+):(?P<minutes>\d+)$')
-sign = lambda x: 1 if x >= 0 else -1
 
 
 def usage():
@@ -35,16 +34,11 @@ def str_to_timedelta(s):
   m = datestr.search(s).groupdict()
   td = timedelta(**{k:int(v) for k, v in m.items()})
   return td
-  #logging.debug("Regexped timedelta = %s" % str(td))
-  #s = sign(td.days)
-  #result = timedelta(days=s*abs(td.days), seconds=s*abs(td.seconds))
-  #logging.debug("Converted to %s" % str(result))
-  #logging.debug("Converted to %s" % timedelta_to_str(result))
-  #return result
 
 
 def timedelta_to_str(td):
-  return "{}d {:02}:{:02}".format(td.days, td.seconds//3600, (td.seconds//60)%60)
+  s = td.seconds
+  return "{}d {:02}:{:02}".format(td.days, s//3600, (s//60)%60)
 
 
 def calc(expr):
